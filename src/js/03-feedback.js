5f3b2d5b161eafaca99c65679f7e26150f8b2619
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-25 17:09:29 +0300

    adds task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
+const email = document.querySelector('[name="email"]');
+const msg = document.querySelector('[name="message"]');
+
+const formFunc = evt => {
+  evt.preventDefault();
+  localStorage.setItem(
+    storageKey,
+    JSON.stringify({ email: email.value, message: msg.value })
+  );
+  console.log(localStorage.getItem(storageKey));
+};
+
+form.addEventListener('submit', throttle(formFunc, 500));
