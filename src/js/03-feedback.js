062cbff19b5b490386254b49187355d706d51dca
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-25 22:39:10 +0300

    adds task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,35 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
 const email = document.querySelector('[name="email"]');
 const msg = document.querySelector('[name="message"]');
+const storageKey = 'feedback-form-state';
+const savedStorage = localStorage.getItem(storageKey);
 
-const formFunc = evt => {
+const formFuncInp = evt => {
   evt.preventDefault();
   localStorage.setItem(
     storageKey,
-    JSON.stringify({ email: email.value, message: msg.value })
+    JSON.stringify({
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    })
   );
+};
+form.addEventListener('input', throttle(formFuncInp, 500));
+
+const formFuncSub = evt => {
+  evt.preventDefault();
   console.log(localStorage.getItem(storageKey));
 };
+form.addEventListener('submit', formFuncSub);
 
-form.addEventListener('submit', throttle(formFunc, 500));
+if (storageKey) {
+  const { email: storageEmail, message: storageMsg } = JSON.parse(savedStorage);
+  email.value = storageEmail;
+  msg.value = storageMsg;
+  //   email.value = JSON.parse(localStorage.getItem(storageKey.message));
+} else {
+  email.value = '';
+  msg.value = '';
+}
