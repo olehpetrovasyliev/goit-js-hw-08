4d90790ba29cdc68cbc83a6de48a665c6484b955
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-26 15:23:54 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,6 +6,16 @@ const msg = document.querySelector('[name="message"]');
 const storageKey = 'feedback-form-state';
 const savedStorage = localStorage.getItem(storageKey);
 
+if (storageKey) {
+  const { email: storageEmail, message: storageMsg } = JSON.parse(savedStorage);
+  email.value = storageEmail;
+  msg.value = storageMsg;
+  //   email.value = JSON.parse(localStorage.getItem(storageKey.message));
+} else {
+  email.value = '';
+  msg.value = '';
+}
+
 const formFuncInp = evt => {
   evt.preventDefault();
   localStorage.setItem(
@@ -26,13 +36,3 @@ const formFuncSub = evt => {
   msg.value = '';
 };
 form.addEventListener('submit', formFuncSub);
-
-if (storageKey) {
-  const { email: storageEmail, message: storageMsg } = JSON.parse(savedStorage);
-  email.value = storageEmail;
-  msg.value = storageMsg;
-  //   email.value = JSON.parse(localStorage.getItem(storageKey.message));
-} else {
-  email.value = '';
-  msg.value = '';
-}
